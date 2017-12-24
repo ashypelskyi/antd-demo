0bb2d255a91865234dc2f1631544c48d256e18f4
ashypelskyi <shypelskyi@gmail.com>
2017-12-25 01:14:18 +0200

    Form is creating when modal window is opening, and is destroying when modal window is hiding

--- src/App.js
+++ src/App.js
@@ -1,19 +1,104 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { Button, Layout, Row, Col, Modal, Form } from 'antd';
+import TestForm from 'components/TestForm'
+import 'App.css';
+
+const { Header, Footer, Content } = Layout;
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isShowModal: false,
+      formRef: null,
+      fields: {
+        username: {
+          value: 'benjycui'
+        },
+        password: {
+          value: '123'
+        },
+        email: {
+          value: `test@test.com`
+        }
+      },
+    };
+  }
+
+  _showModal = () => {
+    const formRef = Form.create({
+      onFieldsChange(props, changedFields) {
+        props.onChange(changedFields);
+      },
+      mapPropsToFields(props) {
+        return {
+          username: Form.createFormField({
+            ...props.username,
+            value: props.username.value,
+          }),
+          password: Form.createFormField({
+            ...props.password,
+            value: props.password.value,
+          }),
+          email: Form.createFormField({
+            ...props.email,
+            value: props.email.value,
+          })
+        };
+      },
+      onValuesChange(_, values) {
+        console.log(values);
+      },
+    })(props => (<TestForm {...props} />));
+
+    this.setState({
+      isShowModal: true,
+      formRef
+    });
+  };
+
+  _hideModal = () => this.setState({ isShowModal: false, formRef: undefined });
+  _handleFormChange = (changedFields) => {
+    this.setState({
+      fields: { ...this.state.fields, ...changedFields },
+    });
+  };
+
   render() {
+    const fields = this.state.fields;
+    const modalConfig = {
+      title: `Test modal form`,
+      onOk: () => { },
+      onCancel: this._hideModal,
+      visible: this.state.isShowModal
+    };
+
+    const CustomForm = this.state.formRef;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Header style={{ color: '#fff', textAlign: 'center', fontSize: '1.5em' }}>
+          Traning version for ant design + React
+        </Header>
+        <Content>
+          <Row gutter={25} style={{ paddingTop: 25 }}>
+            <Col span={11} />
+            <Col span={2}>
+              <Button onClick={this._showModal} type='primary' size='large'>Show modal</Button>
+              <Modal {...modalConfig}>
+                {
+                  CustomForm ?
+                    <CustomForm  {...fields} onChange={this._handleFormChange} /> :
+                    <Fragment />
+                }
+              </Modal>
+            </Col>
+            <Col span={11} />
+          </Row>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     );
   }
 }
---
+++ src/components/TestForm.js
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
+
+class TestForm extends Component {
+    constructor(props) {
+        super(props)
+
+        console.log(`TestForm constructor fire.`);
+    }
+
+    componentWillUnmount() {
+        console.log(`TestForm unmount fire.`);
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+            <Form className="login-form">
+                <FormItem>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                        )
+                    }
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                        )
+                    }
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('email', {
+                        rules: [{ required: true, message: 'Please input your email!' }],
+                    })(
+                        <Input prefix={<Icon type="form" style={{ color: 'rgba(0,0,0,.25)' }} />} type="email" placeholder="Email" />
+                        )
+                    }
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+export default TestForm;
